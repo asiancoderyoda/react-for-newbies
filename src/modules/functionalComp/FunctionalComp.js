baacb9349c5e6ca3189b95a29ee51b9636dbf9f0
asiancoderyoda <avigyanbhaktacontai@gmail.com>
2022-02-09 14:46:55 +0530

    fixes

--- src/modules/functionalComp/FunctionalComp.js
+++ src/modules/functionalComp/FunctionalComp.js
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { fetchPosts } from "../../apis";
 
+const countInitialState = () => {
+    console.log("countInitialState");
+    return 5;
+}
+
 const FunctionalComp = () => {
-    // const [posts, setPosts] = useState(() => {
-    //     return [];
-    // });
-    const [posts, setPosts] = useState([]);
+    // const [posts, setPosts] = useState([]);
+    const [count, setCount] = useState(countInitialState());
+    // const [count, setCount] = useState(() => countInitialState());
 
     useEffect(() => {
-        (async() => {
-            const posts = await fetchPosts();
-            console.log(posts);
-        })();
-    }, [window.location.pathname]);
+        console.log("useEffect");
+
+        return () => {
+            console.log("useEffect cleanup");
+        }
+        
+    }, [window.location.pathname, count]);
+
+    const increment = () => {
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(count + 1);
+        // setCount(prev => prev + 1);
+        // setCount(prev => prev + 1);
+        setCount(prev => prev + 1);
+    }
 
     return (
         <div>
-            <p>Functional Comp Counter</p>
+            <p>{count}</p>
+            <button onClick={increment}>+</button>
         </div>
     );
 }
